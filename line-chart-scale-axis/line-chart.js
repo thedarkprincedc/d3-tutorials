f77bea7cfbd966f74b81af30747906a0614c5fa6
Brett Mosley <brett.mosley@accenturefederal.com>
2018-08-04 14:31:13 -0400

    test

---
+++ line-chart-scale-axis/line-chart.js
@@ -0,0 +1,65 @@
+(function () {
+    var width = 640;
+    var height = 200;
+    var padding = 20;
+    function displayText(value) {
+        d3.select("body")
+            .append("h1")
+            .text(value.name);
+    }
+    function getDate(d){
+        var strDate = new String(d);
+        var year = strDate.substr(0,4);
+        var month = strDate.substr(4,2)-1;
+        var day = strDate.substr(6,2);
+        return new Date(year, month, day)
+    }
+
+    function displayChart(value) {
+        var minDate = getDate(value.data[0]["month"]);
+        var maxDate = getDate(value.data[value.data.length-1]["month"]);
+        console.log("Min Date", minDate)
+        console.log("Max Date", maxDate)
+        let xScale = d3.scaleTime()
+            .domain([ 
+                minDate,
+                maxDate
+            ]).range([padding, width-padding]);
+
+        let yScale = d3.scaleLinear()
+            .domain([0, d3.max(value.data, (v) => { return v.hits; })])
+            .range([height-padding, 10]);
+        let xAxisGen = d3.axisBottom(xScale);
+        let yAxisGen = d3.axisLeft(yScale).ticks(4);
+
+        let linefunc = d3.line()
+            .x((d) => { return xScale(d.month); })
+            .y((d) => { return yScale(d.hits); })
+            .curve(d3.curveLinear);
+
+        let svg = d3.select("body").append("svg")
+            .style("width", width)
+            .style("height", height);
+
+        let xAxis = svg.append("g").call(xAxisGen)
+            .attr("class", "axis")
+            .attr("transform", "translate(0,"+(height-padding)+")");
+
+        let yAxis = svg.append("g").call(yAxisGen)
+                    .attr("class", "axis")
+                    .attr("transform", "translate("+20+",0)");
+
+        let viz = svg.append("path")
+            .attr("d", linefunc(value.data))
+            .attr("stroke", "purple")
+            .attr("stroke-width", 2)
+            .attr("fill", "none")
+            
+    }
+    d3.json("../_data/test.1.json").then((data) => {
+        data.forEach((value, index) => {
+            displayText(value);
+            displayChart(value);
+        })
+    });
+})();
--- line-chart-scale/line-chart.js
+++ line-chart-scale/line-chart.js
@@ -1,37 +1,45 @@
-(function(){
-   var width = 640;
-   var height = 200;
-   function displayText(value){ 
-       d3.select("body")
+(function () {
+    var width = 640;
+    var height = 200;
+
+    function displayText(value) {
+        d3.select("body")
             .append("h1")
             .text(value.name);
-   }
+    }
+    function displayChart(value) {
 
-   function displayChart(value){
-    let xScale = d3.scaleLinear()
-                    .domain([
-                        d3.min(value.data, (v) => { return v.month; }),
-                        d3.max(value.data, (v) => { return v.month; })
-                    ])
-                    .range([0, width]);
-    let yScale = d3.scaleLinear()
-                    .domain([0, d3.max(value.data, (v) => { return v.hits})])
-                    .range([height, 0]);
+        let xScale = d3.scaleLinear()
+            .domain([ 
+                d3.min(value.data, (v) => { return v.month; }),
+                d3.max(value.data, (v) => { return v.month; })
+            ]).range([0, width]);
+
+        let yScale = d3.scaleLinear()
+            .domain([0, d3.max(value.data, (v) => { return v.hits; })])
+            .range([height, 0]);
+
         let linefunc = d3.line()
-            .x( (d) => { return xScale(d.month); })
-            .y( (d) => { return yScale(d.hits); })
-            .curve(d3.curveLinear); 
-        
-        let svg = d3.select("body").append("svg") 
+            .x((d) => {
+                return xScale(d.month);
+            })
+            .y((d) => {
+                return yScale(d.hits);
+            })
+            .curve(d3.curveLinear);
+
+        let svg = d3.select("body").append("svg")
             .style("width", width)
             .style("height", height);
+
         let viz = svg.append("path")
             .attr("d", linefunc(value.data))
             .attr("stroke", "purple")
             .attr("stroke-width", 2)
             .attr("fill", "none")
-   }
-   d3.json("../_data/test.1.json").then((data) => {
+            
+    }
+    d3.json("../_data/test.1.json").then((data) => {
         data.forEach((value, index) => {
             displayText(value);
             displayChart(value);
